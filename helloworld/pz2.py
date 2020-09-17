bc9c729025e6b3b12f3f53c28e7a966402f6cfe6
II03uTuB <sir.med2012@gmail.com>
2020-09-17 18:02:38 +0300

    function is ready

--- helloworld/pz2.py
+++ helloworld/pz2.py
@@ -10,20 +10,37 @@ def shout_on_me():
 def addCalc():
     while True:
         sumOfTwoString = input('Введите данные: \n')
-        if not sumOfTwoString:
-            print('Строка пуста, повторите')
-            continue
         if sumOfTwoString == 'exit':
             exit()
-        if not sumOfTwoString.isspace():
-            print('в строке присутствуют символы, отличные от цифр и знака "+", повторите')
-            continue
+        ptr = True
+        for i in sumOfTwoString:
+            if i.isdigit():
+                continue
+            if i == '+':
+                continue
+            else:
+                print('''В строке присутствуют символы, отличные 
+                                            от цифр и знака "+" или же недостаточно данных для действия, повторите''')
+                ptr = False
+                break
+        if ptr == True:
+            list = sumOfTwoString.split('+')
+            if len(list) > 2:
+                print('Вы ввели больше двух слагаемых')
+                continue
+            print(float(list[0]) + float(list[1]))
 
-        list = sumOfTwoString.split('+')
-        if len(list) > 2:
-            print('Вы ввели больше двух слагаемых')
-            continue
-        print(float(list[0]) + float(list[1]))
+
+#f not sumOfTwoString:
+        #   print('Строка пуста, повторите')
+         #   continue
+
+        #if sumOfTwoString.isalpha():
+        #    print('''В строке присутствуют символы, отличные
+        #    от цифр и знака "+" или же недостаточно данных для действия, повторите''')
+         #   continue
+
+
 
 
 
