994eb4ec220c2660916f17342ec20930ef077332
II03uTuB <sir.med2012@gmail.com>
2020-09-17 18:09:04 +0300

    add a flag

--- helloworld/pz2.py
+++ helloworld/pz2.py
@@ -12,35 +12,25 @@ def addCalc():
         sumOfTwoString = input('Введите данные: \n')
         if sumOfTwoString == 'exit':
             exit()
-        ptr = True
+        flag = True
+        if len(sumOfTwoString) == 0:
+            print('Введена пустая строка, повторите')
+            continue
         for i in sumOfTwoString:
             if i.isdigit():
                 continue
             if i == '+':
                 continue
             else:
-                print('''В строке присутствуют символы, отличные 
-                                            от цифр и знака "+" или же недостаточно данных для действия, повторите''')
-                ptr = False
+                print('''В строке есть символы, отличные от цифр и знака "+", повторите''')
+                flag = False
                 break
-        if ptr == True:
+        if flag:
             list = sumOfTwoString.split('+')
             if len(list) > 2:
                 print('Вы ввели больше двух слагаемых')
                 continue
             print(float(list[0]) + float(list[1]))
-
-
-#f not sumOfTwoString:
-        #   print('Строка пуста, повторите')
-         #   continue
-
-        #if sumOfTwoString.isalpha():
-        #    print('''В строке присутствуют символы, отличные
-        #    от цифр и знака "+" или же недостаточно данных для действия, повторите''')
-         #   continue
-
-
 
 
 
