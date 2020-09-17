451d9a5b8e3e536278b10718436551a3bb7733db
II03uTuB <sir.med2012@gmail.com>
2020-09-17 17:00:10 +0300

    make function

--- helloworld/pz2.py
+++ helloworld/pz2.py
@@ -1,16 +1,17 @@
 
 def shout_on_me():
+    global upperStr
     while True:
         # print('Введите строку')
         str = input('Введите строку: \n')
         if str == 'exit' :
             exit()
-        for x in str:
-            x.upper()
-        print('Полученная строка: ', str)
+        #for x in str:
+        newStr = str.upper()
+        print('Полученная строка: ', newStr)
 
 
 
 
-if __name__ == '__shout_on_me()__':
+if __name__ == '__main__':
     shout_on_me()
