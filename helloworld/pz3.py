548d01644da32226332ef65896c0cfe0d4fe172f
II03uTuB <sir.med2012@gmail.com>
2020-09-18 17:07:47 +0300

    refactor code

--- helloworld/pz3.py
+++ helloworld/pz3.py
@@ -2,12 +2,18 @@ from helloworld.pz2 import *
 
 
 def newParser():
-    functions = {'hello': hello, 'shout_on_me': shout_on_me, 'addCalc': addCalc}
-    # while True:
+    functions = {
+        'hello': hello,
+        'shout_on_me': shout_on_me,
+        'addCalc': addCalc
+    }
+    while True:
     stringWithFunction = input('Введите название функции:')
-    #functions.keys(stringWithFunction)
-    x = functions.get(stringWithFunction)
-    functions[x]
+    #x = functions.get(stringWithFunction)
+    functions[stringWithFunction]()
+    # functions.keys(stringWithFunction)
+
+    functions[x]()
 
     # if stringWithFunction ==
 
