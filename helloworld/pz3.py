1be3d98a11dcd37415bade13126c552a9ccab270
II03uTuB <sir.med2012@gmail.com>
2020-09-18 16:57:32 +0300

    write code

--- helloworld/pz3.py
+++ helloworld/pz3.py
@@ -1,7 +1,16 @@
 from helloworld.pz2 import *
 
+
 def newParser():
-    functions = {'hello': hello(), 'shout_on_me': shout_on_me(), 'addCalc': addCalc()}
-    while True:
-        stringWithFunction = input('Введите название функции:')
-        if stringWithFunction ==
+    functions = {'hello': hello, 'shout_on_me': shout_on_me, 'addCalc': addCalc}
+    # while True:
+    stringWithFunction = input('Введите название функции:')
+    #functions.keys(stringWithFunction)
+    x = functions.get(stringWithFunction)
+    functions[x]
+
+    # if stringWithFunction ==
+
+
+if __name__ == '__main__':
+    newParser()
