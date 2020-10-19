2cdfa08bdd01469a7b293fd66b0926694a5d723b
II03uTuB <sir.med2012@gmail.com>
2020-10-19 18:46:52 +0300

    add ddz

--- helloworld/ddz.py
+++ helloworld/ddz.py
@@ -1,11 +1,27 @@
 from collections import Counter
+import matplotlib.pyplot as plt
+from numpy import save, os
+
+
+def save(name, fmt):
+    pwd = os.getcwd()
+    iPath = '.\\'.format(fmt)
+    if not os.path.exists(iPath):
+        os.mkdir(iPath)
+    os.chdir(iPath)
+    plt.savefig('{}.{}'.format(name, fmt), fmt='png')
+    os.chdir(pwd)
 
 
 def stats(filename):
     with open(filename, 'r', encoding='utf-8') as file:
         strFile = file.read()
-    for c, n in sorted(Counter(strFile).items()):
-        print(u'{} : {}'.format(c, n))
+    result = sorted(Counter(strFile).items())
+    plt.hist(result)
+    plt.title('Statistic')
+    plt.grid(True)
+    save('stats', 'pdf')
+    plt.show()
 
 
 if __name__ == "__main__":
